cb81d6460de9afdadf55788f97144124df768057
TechDanny <kokidanny15@gmail.com>
2023-06-08 14:18:36 -0700

    handle basic operators

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,11 +5,10 @@ if __name__ == "__main__":
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    operators = {"-": sub, "*": mul, "/": div, "+": add}
-    if sys.argv[2] not in list(operators.keys()):
+    signs = {"-": sub, "*": mul, "/": div, "+": add}
+    if sys.argv[2] not in list(signs.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b,
-        operators[sys.argv[2]](a, b)))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, signs[sys.argv[2]](a, b)))
