37815ece67776ab32810f63254a7eac2e6fb3716
TechDanny <kokidanny15@gmail.com>
2023-06-08 14:02:34 -0700

    handle basic operators

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,4 +11,5 @@ if __name__ == "__main__":
         sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, operators[sys.argv[2]](a, b)))
+    print("{} {} {} = {}".format(a, sys.argv[2], b,
+        operators[sys.argv[2]](a, b)))
