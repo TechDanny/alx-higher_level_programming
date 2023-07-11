63418a114c9c903c3de5bcce4e5f42f1686d23d8
TechDanny <kokidanny15@gmail.com>
2023-07-11 12:03:42 -0700

    read content from a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,9 @@ define a function
 
 
 def read_file(filename=""):
+    """
+    open and read content from a file
+    """
     with open("my_file_0.txt", "r", encoding="UTF8") as f:
         for lines in f:
             print(lines, end="")
