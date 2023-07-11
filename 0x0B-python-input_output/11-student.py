fd8c2b6166e87de224d66cf83b1eb94476639233
TechDanny <kokidanny15@gmail.com>
2023-07-11 14:50:36 -0700

    student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+create a class
+"""
+
+
+class Student:
+    """
+    instance attributes
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves dict representation
+        """
+        if attrs == None:
+            return self.__dict__
+        my_dict = {}
+        for x in attrs:
+            try:
+                my_dict[x] = self.__dict__[x]
+            except FileNotFoundError:
+                pass
+        return my_dict
+    def reload_from_json(self, json):
+        """
+        replaces all the attributes of student instance
+        """
+        for m in json:
+            try:
+                setattr(self, m, json[m])
+            except FileNotFoundError:
+                pass
