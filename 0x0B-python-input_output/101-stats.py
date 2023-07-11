0392e209e3ff14697cd029d75f7e2d6f15b474a6
TechDanny <kokidanny15@gmail.com>
2023-07-11 16:17:20 -0700

    log parsing

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import sys
+
+
+def get_status():
+    """
+    show status
+    """
+    numb = 0
+    size = 0
+    fsize = 0
+    status_code = {"200": 0, "301": 0, "400": 0, "401": 0, "402": 0,
+            "403": 0, "404": 0, "405": 0, "500": 0}
+    for n in sys.stdin:
+        file_line = n.split()
+        try:
+            size = int(file_line[-1])
+            cd = file_line[-2]
+            status_codes[cd] = status_codes[cd] + 1
+        except:
+            continue
+        if numb == 9:
+            print("File size: {}".format(size))
+            for k, v in sorted(status_code.items()):
+                if (v != 0):
+                    print("{}: {}".format(k, v))
+            numb = 0
+        numb = numb + 1
+        if numb < 9:
+            print("File size: {}".format(size))
+            for k, v in sorted(status_code.items()):
+                if (v != 0):
+                    print("{}: {}".format(k, v))
+
+if __name__ == "__main__":
+    get_status()
