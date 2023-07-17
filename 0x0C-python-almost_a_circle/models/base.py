66e9ddc80243cf8a47a0b833be2a1b2d80817f3a
TechDanny <kokidanny15@gmail.com>
2023-07-17 06:26:45 -0700

    dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,9 @@ create a class
 """
 
 
+import json
+
+
 class Base:
     """
     define a private class attribute
@@ -19,3 +22,16 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        returns the JSON string representation
+        of list dictionaries
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        if not isinstance(list_dictionaries, list) or not all(type(n) == dict
+           for n in list_dictionaries):
+            raise TypeError("list_dictionaries must be a list of dictionaries")
+        return json.dumps(list_dictionaries)
