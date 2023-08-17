9255baf04eca26c19065969406beabc6f10fd418
TechDanny <kokidanny15@gmail.com>
2023-08-17 04:32:01 -0700

    genre id by show

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,4 @@
+-- imports the database dump
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres
+INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
