39a9bd29d2884feb554a910954ab239034fe49fa
TechDanny <kokidanny15@gmail.com>
2023-08-17 04:39:17 -0700

    genre id for all shows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,3 @@
+-- list all shows contained in the database
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres
+RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
