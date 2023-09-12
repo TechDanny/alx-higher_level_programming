5d86beea8e2ba6757e5c37671b4fb55144d11462
TechDanny <kokidanny15@gmail.com>
2023-09-12 12:59:20 -0700

    Rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (_w, _h) {
+    if (_w > 0 && _h > 0) {
+      this.width = _w;
+      this.height = _h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let n = 0; n < this.width; n++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+};
