1fcaa22a15fb71b5375385d752bcb254791cea16
TechDanny <kokidanny15@gmail.com>
2023-09-29 15:43:02 -0700

    Time for an interview

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -14,9 +14,6 @@ if __name__ == "__main__":
     url = requests.get("https://api.github.com/repos/{}/{}/commits".format(
                        owner_name, repo_name))
     r_file = url.json()
-    try:
-        for n in range(10):
-            print(r_file[n].get('sha'), r_file[i].get('commit').get(
-                  'author').get('name'), sep=": ")
-    except:
-        pass
+    for n in range(10):
+        print(r_file[n].get('sha'), r_file[n].get('commit').get(
+              'author').get('name'), sep=": ")
